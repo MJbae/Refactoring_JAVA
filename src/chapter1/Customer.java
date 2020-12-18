1c98e57c561d66b65245e8745b28cfee0a54a2eb
MJbae <bmj5208@gmail.com>
2020-12-18 16:34:22 +0900

    Refactor: Remove Temp(thisAmount)

--- src/chapter1/Customer.java
+++ src/chapter1/Customer.java
@@ -48,15 +48,13 @@ public class Customer {
 		while (rentals.hasNext()) {
 			Rental each = (Rental) rentals.next();
 
-			double thisAmount = getAmountOf(each);
-
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(getAmountOf(each)) + "\n";
 		}
 
 		// add footer lines
