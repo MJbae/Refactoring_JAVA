de74e48c5323518ae9b29f3e1d2bba5a33304e16
MJbae <bmj5208@gmail.com>
2020-12-18 16:13:24 +0900

    Refactor: Move Method, 같은 데이터 공유하는 클래스도 메소드(getAmountOf) 이동(Customer -> Rental)

--- src/chapter1/Customer.java
+++ src/chapter1/Customer.java
@@ -23,25 +23,7 @@ public class Customer {
 	};
 
 	public double getAmountOf(Rental rental) {
-		double thisAmount = 0;
-
-		// determine amounts for each line
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (rental.getDaysRented() > 2)
-				thisAmount += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (rental.getDaysRented() > 3)
-				thisAmount += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return rental.getAmountOf();
 	}
 
 	public String statement() {
